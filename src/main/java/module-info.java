9da5291b9a96e4d0af7b2d3e69e9e8d1e1b2cba2
RedDotz20 <carlos_tabangay@outlook.com>
2023-05-02 17:01:32 +0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.javafxloginlogout {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+                        
+    opens com.example.javafxloginlogout to javafx.fxml;
+    exports com.example.javafxloginlogout;
+}
