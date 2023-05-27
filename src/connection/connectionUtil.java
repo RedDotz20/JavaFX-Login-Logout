ab2dec8149675e2f496658f843686909c3628450
RedDotz20 <carlos_tabangay@outlook.com>
2023-05-27 21:11:16 +0800

    feat: env add

--- src/connection/connectionUtil.java
+++ src/connection/connectionUtil.java
@@ -2,20 +2,33 @@ package connection;
 
 import java.sql.*;
 import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class connectionUtil {
-    public static Connection connectdb() {
-        String url = "jdbc:mysql://localhost/java-fx-app";
-        String user = "root";
-        String password = "admin";
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, user, password);
-            return conn;
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, e);
-            return null;
-        }
+  public static Connection connectdb() throws SQLException {
+    Properties properties = new Properties();
+    try {
+      FileInputStream inputStream = new FileInputStream(".env");
+      properties.load(inputStream);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    String dbHost = properties.getProperty("DB_HOST");
+    String dbUser = properties.getProperty("DB_USER");
+    String dbPassword = properties.getProperty("DB_PASSWORD");
+    String url = "jdbc:mysql://" + dbHost + "/java-fx-app";
+
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection conn = DriverManager.getConnection(url, dbUser, dbPassword);
+      return conn;
+    } catch (ClassNotFoundException e) {
+      JOptionPane.showMessageDialog(null, e);
+      return null;
+    }
+  }
 }
